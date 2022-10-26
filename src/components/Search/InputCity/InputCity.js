54075f4023b08784ce806e9fc97889c86b608ef1
bynikitachel <nikitavyatiorec@gmail.com>
2022-10-26 13:00:16 +0300

    refactored

--- src/components/Search/InputCity/InputCity.js
+++ src/components/Search/InputCity/InputCity.js
@@ -2,54 +2,15 @@ import React from "react";
 import './inputCity.css';
 
 function InputCity(props) {
-    // console.log(props);
-    //autocomplete
-    const { enterSuggestion, onKeyDown, } = props
-
-    const { activeSuggestion, filteredSuggestions, showSuggestions, userInput } = props.cities
-
-    let suggestionsListComponent;
-
-    if (showSuggestions && userInput) {
-        if (filteredSuggestions.length) {
-            suggestionsListComponent = (
-                <ul className="suggestions">
-                    {filteredSuggestions.map((suggestion, index) => {
-                        let className;
-
-                        // Flag the active suggestion with a class
-                        if (index === activeSuggestion) {
-                            className = "suggestion-active";
-                        }
-
-                        return (
-                            <li className={className} key={suggestion} onClick={enterSuggestion}>
-                                {suggestion}
-                            </li>
-                        );
-                    })}
-                </ul>
-            );
-        } else {
-            suggestionsListComponent = (
-                <div className="no-suggestions">
-                    <em>No suggestions, you're on your own!</em>
-                </div>
-            );
-        }
-    }
-
 
     return (
         <div className="container-city">
             <input
-                onKeyDown={(event) => onKeyDown(event)}
                 autoFocus
                 placeholder="Input city"
                 value={props.value}
                 onChange={props.onChange}>
             </input>
-            {suggestionsListComponent}
         </div>
     )
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -3,10 +3,8 @@ import Search from '../Search/Search'
 import InputCity from '../Search/InputCity/InputCity'
 import SearchCity from '../Search/SearchCity/SearchCity'
 import TableInfo from '../TableInfo/TableInfo'
-import Toggle from '../ui/Toggle'
 import ErrorWindow from '../ErrorWindow/ErrorWindow'
 import { useNavigate, useParams } from "react-router-dom"
-// import Autocomplete from '../Search/InputCity/Autocomplete/Autocomplete'
 
 
 function Weather() {
@@ -17,19 +15,10 @@ function Weather() {
     const [data, setData] = useState({
         value: '',
         responseObj: null,
-        checkedWeat: false,
+        checkedWeather: false,
         error: false
     })
 
-    const [cities, setСities] = useState({
-        activeSuggestion: 0,
-        filteredSuggestions: [],
-        showSuggestions: false,
-        userInput: "",
-        responseObjCities: null,
-        // suggestions: ['1111', '1222', '3333', '44444', '2221']
-        suggestions: []
-    })
 
     useEffect(() => {
         if (!name) {
@@ -41,105 +30,15 @@ function Weather() {
 
     const handleChange = (event) => {
         setData({ ...data, value: event.target.value })
-
-        //autocomplete
-        const { suggestions } = cities;
-        console.log('suggestions-----', Object.values(suggestions))
-
-        const userInput = event.currentTarget.value;
-        console.log('userInput------', userInput);
-
-        const filteredSuggestions = Object.values(suggestions).filter(
-            suggestion => suggestion.toLowerCase().indexOf(userInput.toLowerCase()) === 0 // не понятно
-        )
-        console.log('index----', filteredSuggestions.indexOf(userInput.toLowerCase()));
-        console.log('filteredSuggestions-----', filteredSuggestions);
-
-        setСities({
-            ...cities,
-            activeSuggestion: 0,
-            filteredSuggestions,
-            showSuggestions: true,
-            userInput: event.currentTarget.value
-        })
     }
 
-    const onKeyDown = (event) => {
-        const { activeSuggestion, filteredSuggestions } = cities
-
-        if (event.code === "Enter") {
-            event.preventDefault()
-            event.stopPropagation()
-
-            //autocomplete
-            setСities({
-                ...cities,
-                activeSuggestion: 0,
-                showSuggestions: false,
-                userInput: filteredSuggestions[activeSuggestion]
-            })
-            //
-            getTableInfo(data.value)
-
-        } else if (event.keyCode === 38) {
-            if (activeSuggestion === 0) {
-                return;
-            }
-
-            setСities({ ...cities, activeSuggestion: activeSuggestion - 1 });
-        }
-        // User pressed the down arrow
-        else if (event.keyCode === 40) {
-            if (activeSuggestion - 1 === filteredSuggestions.length) {
-                return;
-            }
-
-            setСities({ ...cities, activeSuggestion: activeSuggestion + 1 });
-        }
-    }
-
-    //autocomplete
-    const enterSuggestion = (event) => {
-        setСities({
-            ...cities,
-            activeSuggestion: 0,
-            filteredSuggestions: [],
-            showSuggestions: false,
-            userInput: event.currentTarget.innerText
-        })
-    }
-
-    const getAutocomlete = () => {
-        fetch("https://raw.githubusercontent.com/aZolo77/citiesBase/master/cities.json")
-            .then(response => {
-                if (response.ok) {
-                    response.json().then((res) => setСities({ ...cities, suggestions: res.city }))
-                    console.log(data.error)
-                } else {
-                    throw new Error('Something went wrong')
-                }
-            })
-            .catch(() => {
-                console.log('errrr')
-            })
-    }
-
-    useEffect(() => {
-        getAutocomlete()
-    }, [cities.suggestions.length])
 
     const changeWeat = () => {
         setData({ ...data, checkedWeat: !data.checkedWeat })
     }
 
     const getTableInfo = (city) => {
-        fetch(`https://community-open-weather-map.p.rapidapi.com/${data.checkedWeat ? 'climate/month' : 'weather'}?q=${city}`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                "x-rapidapi-key": "4e894cd36amsh98aee08d4799ad6p110de9jsn32448b93205d"
-            }
-        })
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f6b0640ae7c87d4a0e1b67c607b45f56`)
             .then(response => {
                 if (response.ok) {
                     response.json().then((res) => setData({ ...data, responseObj: res, error: false }))
@@ -162,8 +61,7 @@ function Weather() {
     return (
         <div>
             <Search
-                InputCity={<InputCity value={data.value} onChange={handleChange} onKeyDown={onKeyDown} cities={cities} enterSuggestion={enterSuggestion} />}
-                Toggle={<Toggle checked={data.checkedWeat} onChange={changeWeat} leftField="day" rightFied="week" />}
+                InputCity={<InputCity value={data.value} onChange={handleChange} />}
                 SearchCity={<SearchCity onClick={() => getTableInfo(data.value)} />}
             />
             {data.error ?
@@ -176,4 +74,4 @@ function Weather() {
     )
 }
 
-export default (Weather)
+export default Weather
